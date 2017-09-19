0a24368f78468ee3e1f4bdb8d3aa54fb2498cfb7
Michael Montesino <mmontesino@live.com>
2017-09-19 15:22:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,11 @@ function destructivelyAddElementToBeginningOfArray() {
   var cars = ['Toyota', 'Ford']
   var moreCars = ['Audi', ...cars]
 }
+function addElementToEndOfArray() {
+  var games = ['Chess', 'Tic Tac Toe']
+  var moreGames = [...games, 'Monopoly']
+}
+function destructivelyAddElementToEndOfArray() {
+  var games = ['Chess', 'Tic Tac Toe']
+  games.push('Checkers')
+}
