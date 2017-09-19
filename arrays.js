58bc2ff01c7da056baa3cc41900ed970dba56f9c
Michael Montesino <mmontesino@live.com>
2017-09-19 15:16:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,11 @@ var chocolateBars = [
   'kitkat',
   'skittles'
 ]
+function addElementToBeginningOfArray () {
+  var cars = ['Toyota', 'Ford']
+  cars.unshift('Audi')
+}
+function destructivelyAddElementToBeginningOfArray() {
+  var cars = ['Toyota', 'Ford']
+  var moreCars = ['Audi', ...cars]
+}
