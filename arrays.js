0a2cdde75408294184d44d14d5a37e3789eaea23
Michael Montesino <mmontesino@live.com>
2017-09-19 15:39:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,3 +20,12 @@ function destructivelyAddElementToEndOfArray() {
   var games = ['Chess', 'Tic Tac Toe']
   games.push('Checkers')
 }
+function accessElementInArray() {
+  var entrepreneurs = ['Elon Musk', 'Steve Jobs', 'Scott Belsky']
+  console.log(entrepreneurs[2])
+}
+function destroctivelyRemoveElementFromBeginningOfArray() {
+  var techCompanies = ['Apple', 'Google', 'Facebook']
+  techCompanies.shift()
+  techCompanies
+}
